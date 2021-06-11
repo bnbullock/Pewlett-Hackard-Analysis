bcde32154ff73697588dc928eabb200dbae19098
bn65bullock <82971038+bn65bullock@users.noreply.github.com>
2021-06-11 18:47:28 -0400

    mentorship eligibility updates

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,3 +1,5 @@
+-- Deliverable #1
+
 --#1 Retrieve emp_no, first_name and last_name from the Employees table
 
 SELECT emp_no, first_name, last_name
@@ -53,3 +55,36 @@ ORDER BY "Count" DESC;
 -- Check the table
 SELECT * FROM retiring_titles;
 
+
+-- Deliverable #2
+
+--#1 Retrieve emp_no, fn, ln and bd from the Employees table
+
+SELECT emp_no, first_name, last_name, birth_date
+FROM employees;
+
+--#2 Retrieve from_date & to_date from the dept_emp table
+--#3 Retrieve the title column from the titles table
+--#4-7 Use distinct on, into, join e and de, join e and t
+--#8-9 filter Filter and order results
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+	e.first_name, 
+	e.last_name, 
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01') AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+order by e.emp_no;
+
+-- Check the table
+SELECT * FROM mentorship_titles;
+
+--#10 Export Mentorship Eligibility table - manually
